c047b5618c75b191c12aeeaa33ea11a5c019b6c1
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-02-05 17:34:51 +0530

    ditched selective validation, will do a general validation of date instead

--- Celeste/Views/Signup.xaml.cs
+++ Celeste/Views/Signup.xaml.cs
@@ -35,89 +35,42 @@ namespace Celeste.Views
             NavigationService.Navigate(new Home());
         }
 
+
+        List<string> months = new List<string>
+        {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        };
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            for(int i = 1; i<=31; ++i)
+            for (int i = 0; i <= 31; ++i)
             {
                 cmb_days.Items.Add(i);
             }
+
+            foreach (string month in months)
+            {
+                cmb_months.Items.Add(month);
+            }
+
+            //ppl need to be >10 years atleast 
+            for (int i = 1900; i <= DateTime.Now.Year - 10; ++i)
+            {
+                cmb_years.Items.Add(i);
+            }
         }
 
-        private void cmb_days_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (Convert.ToInt32(cmb_days.SelectedValue) == 31)
-            {
-                List<string> months = new List<string>
-                {
-                    "January",
-                    "March",
-                    "May",
-                    "July",
-                    "August",
-                    "October",
-                    "December"
-                };
 
-                foreach (string month in months)
-                {
-                    cmb_months.Items.Add(month);
-                }
-            }
-
-            else if (Convert.ToInt32(cmb_days.SelectedValue) == 30)
-            {
-                List<string> months = new List<string>
-                {
-                    "January",
-                    "March",
-                    "April",
-                    "May",
-                    "June",
-                    "July",
-                    "August",
-                    "September",
-                    "October",
-                    "November",
-                    "December"
-                };
-
-                foreach (string month in months)
-                {
-                    cmb_months.Items.Add(month);
-                }
-            }
-
-            else
-            {
-                List<string> months = new List<string>
-                {
-                    "January",
-                    "February",
-                    "March",
-                    "April",
-                    "May",
-                    "June",
-                    "July",
-                    "August",
-                    "September",
-                    "October",
-                    "November",
-                    "December"
-                };
-
-                foreach (string month in months)
-                {
-                    cmb_months.Items.Add(month);
-
-                }
-            
-            }
-
-                if (cmb_days.SelectedValue != null)
-                cmb_months.IsEnabled = true;
-
-            else
-                cmb_months.IsEnabled = false;
-        }
     }
 }
