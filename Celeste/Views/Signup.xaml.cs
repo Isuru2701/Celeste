92e73ec6a7c190787fc89f0c0b62811048a68c31
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-02-05 11:17:50 +0530

    added validation to dates

--- Celeste/Views/Signup.xaml.cs
+++ Celeste/Views/Signup.xaml.cs
@@ -34,5 +34,117 @@ namespace Celeste.Views
         {
 
         }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> months = new List<string>()
+            {
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+              "August",
+              "September",
+              "October",
+              "November",
+              "December" 
+            };
+
+            //cmb_year. lim{1900 -- current.Year}
+            for(int i = 1900; i <= DateTime.Now.Year - 1; ++i) 
+            {
+                cmb_years.Items.Add(i);
+            }
+
+            //cmb_months. lim{January to December}
+            foreach (string month in months)
+            {
+                cmb_months.Items.Add(month);
+            }
+
+            for(int i = 1; i <= 31; ++i)
+            {
+                cmb_days.Items.Add(i);
+            }
+
+
+        }
+
+        private void cmb_days_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Convert.ToInt32(cmb_days.SelectedValue) == 31)
+            {
+                cmb_months.Items.Clear();
+
+                List<string> months = new List<string>()
+            {
+              "January",
+              "March",
+              "May",
+              "July",
+              "August",
+              "October",
+              "December"
+            };
+
+                foreach (string month in months)
+                {
+                    cmb_months.Items.Add(month);
+                }
+            }
+            else
+            {
+                cmb_months.Items.Clear();
+                List<string> months = new List<string>()
+            {
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+              "August",
+              "September",
+              "October",
+              "November",
+              "December"
+            };
+
+                foreach (string month in months)
+                {
+                    cmb_months.Items.Add(month);
+                }
+            }
+        }
+
+        private void cmb_months_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<string> monthswith31 = new List<string>
+            {
+              "January",
+              "March",
+              "May",
+              "July",
+              "August",
+              "October",
+              "December"
+
+            };
+            if (!monthswith31.Any(x => x == Convert.ToString(cmb_months.SelectedValue)))
+            {
+                cmb_days.Items.Clear();
+
+                for (int i = 1; i <= 30; ++i)
+                {
+                    cmb_days.Items.Add(i);
+                }
+
+
+            }
+        }
     }
 }
