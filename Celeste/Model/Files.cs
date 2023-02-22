1f225dd817c5e97e26b4a75ae0988201359ab78e
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-02-22 11:09:12 +0530

    Files dones

--- Celeste/Model/Files.cs
+++ Celeste/Model/Files.cs
@@ -39,20 +39,76 @@ namespace Celeste.Model
             }
         }
 
-        static void Write(string content, string filename)
+        public static void Write(string content, string filename)
         {
-            VerifyDirectory();
             try
             {
+                VerifyDirectory();
                 using (StreamWriter writer = new StreamWriter(appFolderPath + filename))
                 {
                     writer.Write(content);
                 }
             }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("FILE: ACCESS_ERROR: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
             catch (Exception ex)
             {
-                Console.WriteLine("FILE: WRITE_Error: " + ex.Message);
+                Console.WriteLine("FILE: WRITE_ERROR: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+        //reads text files
+        public static string ReadText(string filename)
+        {
+            try
+            {
+                VerifyDirectory();
+
+                return File.ReadAllText(appFolderPath + filename);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("FILE: ACCESS_ERROR: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("FILE: READ_ERORR: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+
+        }
+
+        public static byte[] ReadBytes(string filename)
+        {
+            VerifyDirectory();
+            try
+            {
+
+                   return File.ReadAllBytes(appFolderPath + filename);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("FILE: ACCESS_ERROR: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("FILE: READ_ERORR: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+
+        }
+
+
+        public static bool ResourceExists(string filename)
+        {
+           return File.Exists(appFolderPath + filename);
         }
     }
 }
