40144025d9f992335a3d0b58f14daca2cd559840
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-02-22 10:54:08 +0530

    added verifydir and write functionality to filehandler()

--- Celeste/Model/Files.cs
+++ Celeste/Model/Files.cs
@@ -11,10 +11,11 @@ namespace Celeste.Model
     //Handles interactions with AppData folder
     public static class FileHandler
     {
-        static string appFolderPath;
+        //appdata folder  path
+        public static string appFolderPath {get; set;}
         static FileHandler() 
         {
-            appFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Celeste");
+            appFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Celeste/");
 
         }
 
@@ -36,11 +37,23 @@ namespace Celeste.Model
                 MessageBox.Show("FILE: INTERNAL_ERROR: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
 
             }
-
-
         }
 
+        static void Write(string content, string filename)
+        {
+            VerifyDirectory();
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(appFolderPath + filename))
+                {
+                    writer.Write(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FILE: WRITE_Error: " + ex.Message);
+            }
+        }
     }
-
-
 }
+
