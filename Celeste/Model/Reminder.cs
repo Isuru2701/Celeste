b363d194df77b95059ce55cdc21d5ac4ff0c1424
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-02-24 12:37:39 +0530

    working on notification

--- Celeste/Model/Reminder.cs
+++ Celeste/Model/Reminder.cs
@@ -5,7 +5,11 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
 using Windows.UI.Notifications;
+using Windows.Data;
+using Windows.Foundation;
+using System.Windows;
 
 namespace Celeste.Model
 {
@@ -18,16 +22,35 @@ namespace Celeste.Model
         }   
 
         /// <summary>
-        /// 
+        /// sets daily reminder to the time specified
         /// </summary>
         /// <param name="time"></param>
         public static void SetDailyReminder(AnalogueTime time)
         {
-            //write a save to file
-            FileHandler.Write($"{time:h:mm:ss tt}", reminderfile);
+            try
+            {
 
-            //add notification worker
+                //write a save to file
+                FileHandler.Write($"{time:h:mm:ss tt}", reminderfile);
 
+                //add notification worker
+                var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText02);
+
+                // Fill in the text elements
+                var stringElements = toastXml.GetElementsByTagName("text");
+                stringElements[0].AppendChild(toastXml.CreateTextNode("Title"));
+                stringElements[1].AppendChild(toastXml.CreateTextNode("Content"));
+
+                // Create the toast and attach event listeners
+                var toast = new ToastNotification(toastXml);
+
+                // Show the toast. Be sure to specify the AppUserModelId on your application's shortcut!
+                ToastNotificationManager.CreateToastNotifier(Flow.AppId).Show(toast);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("REMINDER: INTERNAL_ERROR: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         /// <summary>
