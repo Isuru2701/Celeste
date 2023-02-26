65390b3c46c452afcbe8d0fbd5babbb634d40ed2
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-02-26 17:54:29 +0530

    working on graph

--- Celeste/Controls/Chart.xaml.cs
+++ Celeste/Controls/Chart.xaml.cs
@@ -25,6 +25,7 @@ using LiveCharts.Wpf.Charts.Base;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView.VisualElements;
 using SkiaSharp;
+using Celeste.Model;
 
 namespace Celeste.Controls
 {
@@ -37,8 +38,38 @@ namespace Celeste.Controls
         public Chart()
         {
             InitializeComponent();
+        }
 
-         
+        List<DateTime> x_axis = new List<DateTime>();
+        List<Double> y_axis = new List<Double>();
+
+
+        public void LoadData()
+        {
+            Person.GetInstance(Flow.User_ID).FetchScores();
+            foreach(Score score in Person.GetInstance(Flow.User_ID).Scores)
+            {
+                x_axis.Add(score.Date);
+                y_axis.Add(Math.Round(score.Value, 1) * 5);
+            }
+
+        }
+
+        public void Plot()
+        {
+            LoadData();
+            SentimentGraph.AxisX.Add(x_axis);
+
+
+            var series = new SeriesCollection
+                (
+                    new LineSeries
+                    {
+                        Title = "Sentiment Score",
+                        Values = y_axis
+                    }
+                );
+            
         }
     }
 
