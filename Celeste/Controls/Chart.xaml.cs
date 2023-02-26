8429ded242da6e62b74126f82844366cb78e6476
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-02-26 19:32:08 +0530

    WORKING ON GRAPHH RAAAA

--- Celeste/Controls/Chart.xaml.cs
+++ Celeste/Controls/Chart.xaml.cs
@@ -26,6 +26,7 @@ using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView.VisualElements;
 using SkiaSharp;
 using Celeste.Model;
+using System.Reflection.Emit;
 
 namespace Celeste.Controls
 {
@@ -35,43 +36,54 @@ namespace Celeste.Controls
     public partial class Chart : UserControl
     {
 
+
+
         public Chart()
         {
             InitializeComponent();
             LoadData();
+            Plot();
         }
 
         List<string> x_axis = new List<string>();
         List<double> y_axis = new List<double>();
-
 
         public void LoadData()
         {
             Person.GetInstance(Flow.User_ID).FetchScores();
             foreach(Score score in Person.GetInstance(Flow.User_ID).Scores)
             {
-                x_axis.Add(score.Date.ToString("MM/dd"));
+                x_axis.Add(score.Date.ToString("yyyy/MM/dd"));
                 y_axis.Add(Math.Round(score.Value, 1) * 5);
             }
         }
 
-        public ISeries[] Series { get; set; } =
+        public void Plot()
         {
-            new LineSeries<double>
-            {
-                Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
-                Fill = null
-            }
-        };
+            CartesianChart ch = new CartesianChart();
+            ch.Zoom = ZoomingOptions.None;
+            ch.AxisX.Add
+                (
+                    new LiveCharts.Wpf.Axis
+                    {
+                        Title = "Months",
+                        Labels = x_axis,
+                    }
+                );
+            ch.AxisX[0].Separator.StrokeThickness = 0;
+            ch.AxisY[0].Separator.StrokeThickness = 0;
 
-        public LabelVisual Title { get; set; } =
-            new LabelVisual
+            ch.Series = new SeriesCollection
             {
-                Text = "Sentiment Score",
-                TextSize = 25,
-                Padding = new LiveChartsCore.Drawing.Padding(15),
-                Paint = new SolidColorPaint(SKColors.DarkSlateGray)
+                new LineSeries
+                {
+                    Title = "Sentiment",
+                    Values = new ChartValues<double>(y_axis),
+                    Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 255, 255))
+                }
             };
+                    plotter.Children.Add(ch);
+        }
 
     }
 
